a4ee70660261fb0b8e9a4b01c4a4c129a92a4ab2
CNevd <wangyalong20@gmail.com>
2017-08-24 11:11:30 +0800

    sparse usr data

--- DSSM/DataIter.py
+++ DSSM/DataIter.py
@@ -4,42 +4,42 @@ import mxnet as mx
 
 
 class DataIter(mx.io.DataIter):
-    def __init__(self, file_path, batch_size, doc_dim):
+    def __init__(self, file_path, batch_size, usr_num, doc_dim):
         super(DataIter, self).__init__()
         self.batch_size = batch_size
+        self.usr_num = usr_num
         self.file_path = file_path
-        #self.files = os.listdir(self.file_path)
-        self.files = [self.file_path]
+        self.files = os.listdir(self.file_path)
         self.index = 0
-        self.provide_data = [('data_usr',(self.batch_size,)),
+        self.provide_data = [('data_usr',(self.batch_size, self.usr_num)),
                              ('doc_pos',(self.batch_size, doc_dim)),
                              ('doc_neg',(self.batch_size, doc_dim))]
+        self.label = [mx.nd.zeros(shape=(self.batch_size,))]
         self.provide_label = [('mae_label',(self.batch_size,))]
         # Load news vector
         self.news_vec = np.load('./dssm_data/dssm/s_titledata.npy')
 
     def __iter__(self):
-        usr_data = []
-        doc_pos = []
-        doc_neg = []
-        label = [mx.nd.zeros(shape=(self.batch_size,))]
         for f in self.files:
             fname = os.path.join(self.file_path, f)
-            fname =f
             data = np.load(fname)
             self.index_start = 0
             size = data.shape[0]
+            idx = np.arange(size)
+            np.random.shuffle(idx)
             while(self.index_start + self.batch_size < size):
                 index_end = self.index_start + self.batch_size
-                usr_data = data[self.index_start : index_end, 0].astype(int)
+                uid = data[self.index_start : index_end, 0].astype(int)
+                usr_data = mx.nd.csr_matrix(np.ones(self.batch_size), np.arange(self.batch_size+1), uid, (self.batch_size, self.usr_num))
                 doc_pos = self.news_vec[data[self.index_start : index_end, 2]]
                 doc_neg = self.news_vec[data[self.index_start : index_end, 3]]
-                data_all = [mx.nd.array(usr_data), mx.nd.array(doc_pos), mx.nd.array(doc_neg)]
+                data_all = [usr_data, mx.nd.array(doc_pos), mx.nd.array(doc_neg)]
                 data_names = ['data_usr', 'doc_pos', 'doc_neg']
                 self.index_start = index_end
-                data_batch = mx.io.DataBatch(data_all, label,
+                data_batch = mx.io.DataBatch(data_all, self.label,
                                              provide_data=self.provide_data,
                                              provide_label=self.provide_label)
                 yield data_batch
-        return
 
+    def reset(self):
+        pass
--- DSSM/dssm.py
+++ DSSM/dssm.py
@@ -1,4 +1,5 @@
 import sys,os
+import time
 import numpy as np
 import mxnet as mx
 from DataIter import DataIter
@@ -10,8 +11,9 @@ logging.basicConfig(level=logging.DEBUG)
 USR_NUM = 2953334
 DOC_DIM = 200
 OUT_DIM = 128
-batch_size = 1024
+batch_size = 2048
 num_hidden = 512
+num_epoch = 1
 
 
 class Cosine(mx.metric.EvalMetric):
@@ -23,11 +25,23 @@ class Cosine(mx.metric.EvalMetric):
       self.sum_metric += pred.asnumpy().mean()
       self.num_inst += 1
 
+class Acc(mx.metric.EvalMetric):
+  def __init__(self):
+    super(Acc, self).__init__('acc')
+
+  def update(self, label, preds):
+    for pred in preds:
+      pred_np = pred.asnumpy()
+      self.sum_metric += (pred_np<0.693147181).sum()
+      self.num_inst += len(pred_np)
+
 def get_dssm():
   doc_pos = mx.sym.Variable('doc_pos')
   doc_neg = mx.sym.Variable('doc_neg')
-  data_usr = mx.sym.Variable("data_usr")
+  data_usr = mx.sym.Variable("data_usr", stype='csr')
 
+  #with mx.AttrScope(ctx_group="cpu"):
+  w_usr = mx.sym.Variable('usr_weight', stype='row_sparse', shape=(USR_NUM, OUT_DIM))
   # shared weights
   w1 = mx.sym.Variable('fc1_doc_weight')
   w2 = mx.sym.Variable('fc2_doc_weight')
@@ -52,9 +66,9 @@ def get_dssm():
     return fc3
 
   # usr net
-  usr = mx.sym.Embedding(data=data_usr, input_dim=USR_NUM, output_dim=OUT_DIM, name='usr_embbed')
-  usr = mx.sym.Flatten(data=usr)
-  usr = mx.sym.L2Normalization(data=usr)
+  #with mx.AttrScope(ctx_group="cpu"):
+  usr1 = mx.sym.dot(data_usr, w_usr)
+  usr = mx.sym.L2Normalization(data=usr1)
   # doc net
   mlp_pos = doc_mlp(doc_pos)
   mlp_neg = doc_mlp(doc_neg)
@@ -72,48 +86,94 @@ def train(ctx):
   logger.setLevel(logging.INFO)
 
   # Load data
-  train_iter = DataIter('./dssm_data/dssm/0', batch_size, DOC_DIM)
+  train_iter = DataIter('./dssm_data/dssm/0', batch_size, USR_NUM, DOC_DIM)
   data_names = [k[0] for k in train_iter.provide_data]
   label_names = [k[0] for k in train_iter.provide_label]
 
   # Set symbol
   dssm = get_dssm()
   arg_names = dssm.list_arguments()
-  arg_shape, out_shape, aux_shape = dssm.infer_shape(data_usr = (batch_size,),
+  arg_shape, out_shape, aux_shape = dssm.infer_shape(data_usr = (batch_size, USR_NUM),
                                                      doc_pos  = (batch_size, DOC_DIM),
                                                      doc_neg  = (batch_size, DOC_DIM))
   arg_shape_dict = dict(zip(arg_names, arg_shape))
   print "DSSM: ", arg_shape_dict
 
   # Module
-  mod = mx.module.Module(dssm, data_names=data_names, label_names=label_names, logger=logger, context=ctx)
+  kv = mx.kvstore.create('local')
+  mod = mx.mod.Module(symbol=dssm, data_names=data_names, label_names=label_names, logger=logger, context=ctx)
+  mod.bind(data_shapes=train_iter.provide_data, label_shapes=train_iter.provide_label)
+  mod.init_params(initializer=mx.init.Uniform(scale=.05))
+  #mod.init_params(mx.init.Xavier(factor_type="in", magnitude=2.34))
+  sgd = mx.optimizer.Adam(learning_rate=0.01, rescale_grad=1.0/batch_size)
+  mod.init_optimizer(optimizer=sgd, kvstore=kv)
 
-  opt_params = {'wd': 0.01,
-                'learning_rate': 0.01}
-  batch_end_callback = mx.callback.Speedometer(batch_size, frequent=1)
+  # Metric and Callback
+  metric = mx.metric.CompositeEvalMetric()
+  metric.add(Cosine())
+  metric.add(Acc())
+  batch_end_callback = mx.callback.Speedometer(batch_size, frequent=50)
   epoch_end_callback = mx.callback.do_checkpoint('dssm', period=1)
-  kv = 'device'
 
-  #mx.profiler.profiler_set_config(mode='all', filename='profile_output.json')
-  #mx.profiler.profiler_set_state('run')
-  def asum_stat(x):
-      return x
-  #monitor = mx.mon.Monitor(1, stat_func=asum_stat, pattern=".*")
-  monitor = None
+  # Train
+  logging.debug('start training ...')
+  for epoch in range(num_epoch):
+    tic = time.time()
+    metric.reset()
+    nbatch = 0
+    data_iter = iter(train_iter)
+    end_of_batch = False
+    next_batch = next(data_iter)
+    while not end_of_batch:
+      batch = next_batch
+      # Get row ids for devices
+      if (len(ctx) > 1):
+        mx.module.executor_group._load_data(batch, 
+                                            mod._exec_group.data_arrays,
+                                            mod._exec_group.data_layouts)
+        data_num = len(mod._exec_group.data_arrays[0])
+        row_ids = [mod._exec_group.data_arrays[0][i][1].indices.copyto(mx.cpu())
+                     for i in range(data_num)]
+      else:
+        row_ids = [batch.data[0].indices.copyto(mx.cpu())]
 
-  mod.fit(train_iter, eval_metric=Cosine(),
-          batch_end_callback=batch_end_callback,
-          epoch_end_callback=epoch_end_callback,
-          kvstore=kv,
-          optimizer='adadelta', optimizer_params=opt_params,
-          initializer=mx.init.Xavier(factor_type="in", magnitude=2.34),
-          num_epoch=1,
-          monitor=monitor)
+      # pull sparse weight
+      index = mod._exec_group.param_names.index('usr_weight')
+      kv.row_sparse_pull('usr_weight', mod._exec_group.param_arrays[index],
+                          priority=-index, row_ids=row_ids)
+      mod.forward_backward(batch)
+      # update parameters
+      mod.update()
+      try:
+          # pre fetch next batch
+          next_batch = next(data_iter)
+          mod.prepare(next_batch)
+      except StopIteration:
+          end_of_batch = True
+      # accumulate metric
+      mod.update_metric(metric, batch.label)
+      batch_end_params = mx.model.BatchEndParam(epoch=epoch, nbatch=nbatch,
+                                                eval_metric=metric,
+                                                locals=locals())
+      batch_end_callback(batch_end_params)
+      nbatch += 1
 
-  #mx.profiler.profiler_set_state('stop')
+    # one epoch of training is finished
+    for name, val in metric.get_name_value():
+      logger.info('Epoch[%d] Train-%s=%f', epoch, name, val)
+      toc = time.time()
+      logger.info('Epoch[%d] Time cost=%.3f', epoch, (toc-tic))
+
+    # sync aux params across devices
+    arg_params, aux_params = mod.get_params()
+    mod.set_params(arg_params, aux_params)
+    epoch_end_callback(epoch, mod.symbol, arg_params, aux_params)
+
+    train_iter.reset()
+
 
 
 if __name__ == '__main__':
-  gpus = "0" # change to None to use cpu
+  gpus = '0' # change to None to use cpu
   ctx = mx.cpu() if gpus is None else [mx.gpu(int(i)) for i in gpus.split(',')]
   train(ctx)
